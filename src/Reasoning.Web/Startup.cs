75bda55c878d859e76d69addd3fe174d0cd42215
mitsbits <mitsbits@gmail.com>
2016-12-07 14:28:05 +0200

    adding libraries

--- src/Reasoning.Web/Startup.cs
+++ src/Reasoning.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CryptoHelper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -9,8 +10,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OpenIddict;
 using Reasoning.Web.Data;
 using Reasoning.Web.Models;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 namespace Reasoning.Web
 {
@@ -31,10 +35,22 @@ namespace Reasoning.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var appSqlConnString = Configuration["Data:app:ConnectionString"];
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            Log.Logger = log;
+            services.AddSingleton<ILogger>(log);
+
+
+
             services.AddMvc();
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration["Data:app:ConnectionString"]));
+                options.UseSqlServer(appSqlConnString));
 
             services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -47,15 +63,30 @@ namespace Reasoning.Web
                 // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
                 .AddMvcBinders()
 
-                // Enable the token endpoint (required to use the password flow).
+                // Enable the authorization, logout, token and userinfo endpoints.
+                .EnableAuthorizationEndpoint("/connect/authorize")
+                .EnableLogoutEndpoint("/connect/logout")
                 .EnableTokenEndpoint("/connect/token")
+                .EnableUserinfoEndpoint("/Account/Userinfo")
 
-                // Allow client applications to use the grant_type=password flow.
+                // Note: the Mvc.Client sample only uses the code flow and the password flow, but you
+                // can enable the other flows if you need to support implicit or client credentials.
+                .AllowAuthorizationCodeFlow()
                 .AllowPasswordFlow()
                 .AllowRefreshTokenFlow()
 
+                // Make the "client_id" parameter mandatory when sending a token request.
+                .RequireClientIdentification()
+
                 // During development, you can disable the HTTPS requirement.
                 .DisableHttpsRequirement()
+
+                // When request caching is enabled, authorization and logout requests
+                // are stored in the distributed cache by OpenIddict and the user agent
+                // is redirected to the same page with a single parameter (request_id).
+                // This allows flowing large OpenID Connect requests even when using
+                // an external authentication provider like Google, Facebook or Twitter.
+                .EnableRequestCaching()
 
                 // Register a new ephemeral key, that is discarded when the application
                 // shuts down. Tokens signed using this key are automatically invalidated.
@@ -64,10 +95,14 @@ namespace Reasoning.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+            loggerFactory.AddSerilog();
+
+            // Ensure any buffered events are sent at shutdown
+            lifetime.ApplicationStopped.Register(Log.CloseAndFlush);
 
             if (env.IsDevelopment())
             {
@@ -80,7 +115,17 @@ namespace Reasoning.Web
             }
 
             app.UseStaticFiles();
-
+            app.UseIdentity();
+            app.UseOAuthValidation();
+            app.UseOpenIddict();
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                RequireHttpsMetadata = false,
+                Audience = "http://localhost:28667/",
+                Authority = "http://localhost:28667/"
+            });
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -90,6 +135,55 @@ namespace Reasoning.Web
                     name: "spa-fallback",
                     defaults: new { controller = "angapp", action = "Index" });
             });
+
+            using (var context = new ApplicationDbContext(app.ApplicationServices.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                context.Database.EnsureCreated();
+
+                // Add Mvc.Client to the known applications.
+                if (!context.Applications.Any())
+                {
+                    // Note: when using the introspection middleware, your resource server
+                    // MUST be registered as an OAuth2 client and have valid credentials.
+                    // 
+                    // context.Applications.Add(new OpenIddictApplication {
+                    //     Id = "resource_server",
+                    //     DisplayName = "Main resource server",
+                    //     Secret = Crypto.HashPassword("secret_secret_secret"),
+                    //     Type = OpenIddictConstants.ClientTypes.Confidential
+                    // });
+
+                    context.Applications.Add(new OpenIddictApplication
+                    {
+                        ClientId = "Reasoning.Web",
+                        ClientSecret = Crypto.HashPassword("secret_secret_secret"),
+                        DisplayName = "Reasoning.Web",
+                        LogoutRedirectUri = "http://localhost:28667/",
+                        RedirectUri = "http://localhost:28667/signin-oidc",
+                        Type = OpenIddictConstants.ClientTypes.Confidential
+                    });
+
+                    // To test this sample with Postman, use the following settings:
+                    // 
+                    // * Authorization URL: http://localhost:54540/connect/authorize
+                    // * Access token URL: http://localhost:54540/connect/token
+                    // * Client ID: postman
+                    // * Client secret: [blank] (not used with public clients)
+                    // * Scope: openid email profile roles
+                    // * Grant type: authorization code
+                    // * Request access token locally: yes
+                    context.Applications.Add(new OpenIddictApplication
+                    {
+                        ClientId = "postman",
+                        DisplayName = "Postman",
+                        RedirectUri = "https://www.getpostman.com/oauth2/callback",
+                        Type = OpenIddictConstants.ClientTypes.Public
+                    });
+
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
+
